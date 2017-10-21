02fcc97c2f0c314b7805eaba90f2f6f10c1afa27
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-10-21 13:58:38 -0400

    Edit client to allow 2 player without using command line arguments.

---
+++ client.py
@@ -0,0 +1,46 @@
+import sys
+import json
+from Game import Game
+
+if (sys.version_info > (3, 0)):
+    print("Python 3.X detected")
+    import socketserver as ss
+else:
+    print("Python 2.X detected")
+    import SocketServer as ss
+
+class NetworkHandler(ss.StreamRequestHandler):
+    def get_msg(self):
+        # reads until '\n' encountered
+        return json.loads(str(self.rfile.readline().decode()))
+
+    def send_msg(self, response):
+        self.wfile.write((json.dumps(response, separators=(',', ':')) + '\n').encode())
+
+    def handle(self):
+        game = Game()
+        while True:
+            game.parse_msg(self.get_msg())
+            response = game.get_cmd()
+            if response:
+                self.send_msg(response)
+            else:
+                print('Response Empty!')
+
+
+# TODO Tell Game() that it should use an agent with a certain strategy. For
+# testing purposes.
+def main():
+    host = '127.0.0.1'
+    port = 9090
+    try:
+        server = ss.TCPServer((host, port), NetworkHandler)
+    except OSError:
+        port += 1
+        server = ss.TCPServer((host, port), NetworkHandler)
+    print("listening on {}:{}".format(host, port))
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
